7530ba70d4ed5c1f816fb6f71666f7bfcff5245f
JEISSON CARDENAS <jeicar15@hotmail.com>
2022-11-11 08:01:17 -0500

    modificacion interfaces

--- src/components/admin/Home.js
+++ src/components/admin/Home.js
@@ -3,114 +3,114 @@ import ListProducts from "../products.json";
 
 export const Home = () => {
 
-    const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([])
 
-    useEffect(() => { 
-        setProducts(ListProducts['productos'])
-        console.log(products)
-    }, [])
+  useEffect(() => { 
+      setProducts(ListProducts['productos'])
+      console.log(products)
+  }, [])
 
 
-  const saveProduct = (e) => {
-    e.preventDefault();
-    const product = {
-      id: Math.random(),
-      name: e.target.name.value,
-      image: e.target.image.value,
-      features: e.target.features.value,
-      price: e.target.price.value,
-      description: e.target.description.value,
-    };
-    
-    setProducts([...products, product]);
-    console.log(products);
-
-    alert("Producto agregado");
+const saveProduct = (e) => {
+  e.preventDefault();
+  const product = {
+    id: Math.random(),
+    name: e.target.name.value,
+    image: e.target.image.value,
+    features: e.target.features.value,
+    price: e.target.price.value,
+    description: e.target.description.value,
   };
+  
+  setProducts([...products, product]);
+  console.log(products);
 
-  const deleteProduct = (id) => {
-    const newProducts = products.filter((product) => product.id !== id);
-    setProducts(newProducts);
-  };
-  return (
-    <>
-      <div className="container py-4">
-        <h3>Admin Home</h3>
-        <div className="row">
-          <div className="col-md-4">
-            <form onSubmit={saveProduct}>
-              <label for="name">Name</label>
-              <input
-                className="form-control"
-                type="text"
-                name="name"
-                id="name"
-              />
+  alert("Producto agregado");
+};
 
-              <label for="features">Features</label>
-              <input
-                className="form-control"
-                type="text"
-                name="features"
-                id="features"
-              />
+const deleteProduct = (id) => {
+  const newProducts = products.filter((product) => product.id !== id);
+  setProducts(newProducts);
+};
+return (
+  <>
+    <div className="container py-4">
+      <h3>Admin Home</h3>
+      <div className="row">
+        <div className="col-md-4">
+          <form onSubmit={saveProduct}>
+            <label for="name">Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              id="name"
+            />
 
-              <label for="price">Price</label>
-              <input
-                className="form-control"
-                type="text"
-                name="price"
-                id="price"
-              />
+            <label for="features">Features</label>
+            <input
+              className="form-control"
+              type="text"
+              name="features"
+              id="features"
+            />
 
-              <label for="image">Image</label>
-              <input
-                className="form-control"
-                type="text"
-                name="image"
-                id="image"
-              />
+            <label for="price">Price</label>
+            <input
+              className="form-control"
+              type="text"
+              name="price"
+              id="price"
+            />
 
-              <label for="description">Description</label>
-              <textarea
-                className="form-control"
-                name="description"
-                id="description"
-              ></textarea>
+            <label for="image">Image</label>
+            <input
+              className="form-control"
+              type="text"
+              name="image"
+              id="image"
+            />
 
-              <button className="btn btn-primary mt-4" type="submit">
-                Add Product
-              </button>
-            </form>
-          </div>
-          <div className="col-md-8">
-            <h3>Products</h3>
+            <label for="description">Description</label>
+            <textarea
+              className="form-control"
+              name="description"
+              id="description"
+            ></textarea>
 
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Price</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  products.map((product) => (
-                    <tr key={product.id}> 
-                      <td>{product.name}</td>
-                      <td>{product.price}</td>
-                      <td>
-                        <button className="btn btn-danger" onClick={() => deleteProduct(product.id)}>Delete</button>
-                      </td>
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
-          </div>
+            <button className="btn btn-primary mt-4" type="submit">
+              Add Product
+            </button>
+          </form>
+        </div>
+        <div className="col-md-8">
+          <h3>Products</h3>
+
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                products.map((product) => (
+                  <tr key={product.id}> 
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>
+                      <button className="btn btn-danger" onClick={() => deleteProduct(product.id)}>Delete</button>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
         </div>
       </div>
-    </>
-  );
+    </div>
+  </>
+);
 };
--- src/components/admin/Sales.js
+++ src/components/admin/Sales.js
@@ -3,49 +3,49 @@ import SalesJson from '../ventas.json'
 
 const Sales = () => {
 
-    const [sales, setSales] = useState([]);
+  const [sales, setSales] = useState([]);
 
-    useEffect(() => {
-        setSales(SalesJson)
-        console.log(sales)
-    }, [])
+  useEffect(() => {
+      setSales(SalesJson)
+      console.log(sales)
+  }, [])
 
 
 
-  return (
-    <>
-        <div className="container py-4">
-            <h3>Sales</h3>
+return (
+  <>
+      <div className="container py-4">
+          <h3>Sales</h3>
 
-            <div className="row">
-                <div className="col-md-8">
-                <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Price</th>
-                  <th>Cantidad</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  sales.map((product) => (
-                    <tr key={product.id}> 
-                      <td>{product.producto}</td>
-                      <td>{product.precio}</td>
-                      <td>{product.cantidad}</td>
-                    <td></td>
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
-            </div>
-            </div>
-        </div>
-        
-    </>
-  )
+          <div className="row">
+              <div className="col-md-8">
+              <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Cantidad</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                sales.map((product) => (
+                  <tr key={product.id}> 
+                    <td>{product.producto}</td>
+                    <td>{product.precio}</td>
+                    <td>{product.cantidad}</td>
+                  <td></td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+          </div>
+          </div>
+      </div>
+      
+  </>
+)
 }
 
 export default Sales
